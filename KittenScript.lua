b987c2ed636e772aea3c837a226cfbba99ec5720
Volume <seungho081027@gmail.com>
2024-12-01 16:51:22 +0900

    V4 Update
    V4 Update

--- KittenScript.lua
+++ KittenScript.lua
@@ -3,31 +3,22 @@
         Original Developer: @icedoomfist / https://github.com/IceDoomfist
         MEOW
     ]]--
-
     --[[
-    TODO: 오비탈 / 기타 등등 글로벌 변수화
-          SKR 기능 이전
-          설정저장
-          돈추가 카운터 (오류발생시 추가 X 포함? - Errorhash이용)
-          메뉴 div 추가 및 탭 이름들 바꾸기
-          번역기능 추가
-    ]]--
-
-    --[[
-    V3 변경사항:
-    Earn Orbital - 루프 오류 해결
+    V4 변경사항:
+       얻는 금액 제한 추가
+       딜레이 간편화
+       간편한 언락, 레벨변경 추가
     ]]--
 --------
 
 main_var = {
-    SCRIPT_NAME = "Mewo",
-    SCRIPT_VER = 3,
+    SCRIPT_NAME = "Kitten",
+    SCRIPT_VER = 4,
 
     delay_Type = 1,
     delay_sec = {
         delay_1 = 500,
-        delay_2 = 1000,
-        delay_3 = 10000
+        delay_2 = 11500
     },
 
     orbital_flag = {
@@ -49,6 +40,16 @@ main_var = {
         util.joaat("CTALERT_F_3"),
         util.joaat("CTALERT_F_4"),
     },
+
+    limit_flag = {
+        limit_selection = 50000000,
+        got = 0,
+        is_limit = true,
+    },
+
+    fast_change = {
+        rp_selection = 120
+    },
 }
 
 labels = {
@@ -63,7 +64,7 @@ labels = {
         custom_tab = "Custom Money",
 
         orbital_menu = "[$] Earn Orbital Money",
-        orbital_desc = "slowest but safest and stable method, not limited!!\nflag_500: delay 1\nflag_11500: delay 1 + delay2 + delay3",
+        orbital_desc = "slowest but safest and stable method, not limited!!\nflag_500: delay1, flag_11500: delay4",
 
         ez680k_menu = "[$] Earn 680k",
         ez180k_menu = "[$] Earn 180k",
@@ -77,7 +78,29 @@ labels = {
         misc_div = "Misc",
 
         trans_atskp_menu = "Auto Skip Transaction Error",
-        trans_atskp_desc = "It won't prevent errors, but it will automatically skip the error screen.\nthanks to jinx"
+        trans_atskp_desc = "It won't prevent errors, but it will automatically skip the error screen.\nthanks to jinx",
+
+        fast_unlock = "Fast Unlock All",
+        fast_unlock_desc = ":)",
+
+        fast_change_rp_div = "Fast Change RP",
+
+        fast_rp_sel_name = "Rank: ",
+        fast_rp_sel_desc = ":)",
+        fast_do_rp = "Change RP",
+        fast_do_rp_desc = "Max: 8,000",
+        fast_do_rep = "Change LS Rep",
+        fast_do_rep_desc = "Max: 1,000",
+
+        limit_div = "Earn limit",
+        
+        limit_sel = "Limit: ",
+        limit_sel_de = "Default: $50,000m",
+
+        reset_earn = "Reset Earned",
+        
+        limit_enable = "Enable",
+        limit_enable_desc = "The loop will automatically turn off when you reach the amount you set."
     },
 
     Setting_labels = {
@@ -103,6 +126,8 @@ labels = {
         delay_custom_select_2_desc = "Default: 1000ms",
         delay_custom_select_3 = "Select Delay 3: ",
         delay_custom_select_3_desc = "Default: 10000ms", 
+        delay_custom_select_4 = "Select Delay 4: ",
+        delay_custom_select_4_desc = "Default: 11500ms", 
         
         overlay_div = "Watermark Setting",
 
@@ -116,8 +141,8 @@ labels = {
         credit = "@volalume @icedoomfist",
 
         orbital_wlf = {
-            flag_500 = "orbital_flag: waiting_"..main_var.delay_sec.delay_1.."ms",
-            flag_11500 = "orbital_flag: waiting_"..main_var.delay_sec.delay_1 + main_var.delay_sec.delay_2 + main_var.delay_sec.delay_3.."ms",
+            flag_500 = "orbital_flag: waiting_delay_1",
+            flag_11500 = "orbital_flag: waiting_delay_2",
             flag_turndown = "orbital_flag: turning down",
             flag_none = "orbital_flag: not running"
         },
@@ -130,14 +155,18 @@ labels = {
 
             autoskip_on = "Error AutoSkip: on",
             autoskip_off = "Error AutoSkip: off"
-        },
-
+        },   
         
+       limit_wlf = {
+            limited_earn = "Limited Earn: "..main_var.limit_flag.limit_selection,
+            your_earned = "You Earned: "..main_var.limit_flag.got,
+            is_limit_wlf = "Limit: "..main_var.limit_flag.is_limit
+        },  
     },
 
-    System_label = {
-        --loopblock = "Only one loop can be active"
-    }
+    Flag_label = {
+        done = "done"
+    },
 }
 
 global_var = {
@@ -214,40 +243,56 @@ menu.divider(menu.my_root(), labels.main_root)
 ---#Menu
 Money = menu.list(menu.my_root(), labels.Money_labels.money_tab, {"makemoney"}, "", function(); end)
 
-menu.divider(Money, labels.Money_labels.money_div)
-Money_instant = menu.list(Money, labels.Money_labels.instant_tab, {"instantmoney"}, "", function(); end)
-Money_safe = menu.list(Money, labels.Money_labels.safe_tab, {"safemoney"}, "", function(); end)
-Money_custom = menu.list(Money, labels.Money_labels.custom_tab, {"custommoney"}, "", function(); end)
-
 Misc = menu.list(menu.my_root(), labels.Misc_labels.misc_tab, {"misc"}, "", function(); end)
 
 Setting = menu.list(menu.my_root(), labels.Setting_labels.setting_tab, {"setting"}, "", function(); end)
 --Delay = menu.list(Setting, "Delay Setting", {"delaysetting"}, "", function(); end) / Line 133
 
 ---#Instant Money
-menu.toggle_loop(Money_instant, labels.Money_labels.orbital_menu, {"orbtloop"}, labels.Money_labels.orbital_desc, function() --TODO: Fixed on V3
+menu.toggle_loop(Money, labels.Money_labels.orbital_menu, {"orbtloop"}, labels.Money_labels.orbital_desc, function() --TODO: Fixed on V3
     main_var.loop_flag.is_loop_running = true
     labels.Watermark_label.loop_wlf.running_name = labels.Money_labels.orbital_menu
 
-    set_global_i(global_var.instant.orbital, 1)
+    if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+        util.toast(labels.Flag_label.done)
+        menu.trigger_commands("orbtloop off")
+    else
+        set_global_i(global_var.instant.orbital, 1)
+    end
     main_var.orbital_flag.waiting_500 = true
     util.yield(main_var.delay_sec.delay_1)
     main_var.orbital_flag.waiting_500 = false
 
-    set_global_i(global_var.instant.orbital, 0)
+    if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+        util.toast(labels.Flag_label.done)
+        menu.trigger_commands("orbtloop off")
+    else
+        set_global_i(global_var.instant.orbital, 0)
+        main_var.limit_flag.got = main_var.limit_flag.got + 500000
+    end
     main_var.orbital_flag.waiting_11500 = true;
-    util.yield(main_var.delay_sec.delay_1 + main_var.delay_sec.delay_2 + main_var.delay_sec.delay_3)
+    util.yield(main_var.delay_sec.delay_2)
     main_var.orbital_flag.waiting_11500 = false;
 
-    set_global_i(global_var.instant.orbital, 2)
+    if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+        util.toast(labels.Flag_label.done)
+        menu.trigger_commands("orbtloop off")
+    else
+        set_global_i(global_var.instant.orbital, 2)
+    end
     main_var.orbital_flag.waiting_500 = true
     util.yield(main_var.delay_sec.delay_1)
     main_var.orbital_flag.waiting_500 = false
 
-    set_global_i(global_var.instant.orbital, 0)
+    if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+        util.toast(labels.Flag_label.done)
+        menu.trigger_commands("orbtloop off")
+    else
+        set_global_i(global_var.instant.orbital, 0)
+        main_var.limit_flag.got = main_var.limit_flag.got + 750000
+    end
     main_var.orbital_flag.waiting_11500 = true;
-    util.yield(main_var.delay_sec.delay_1 + main_var.delay_sec.delay_2 + main_var.delay_sec.delay_3)
-    main_var.orbital_flag.waiting_11500 = false;
+    util.yield(main_var.delay_sec.delay_2)
 end, function()
     main_var.orbital_flag.waiting_500 = false
     main_var.orbital_flag.waiting_11500 = false;
@@ -259,19 +304,37 @@ end, function()
     do_ezmoney(global_var.ezmoney_hash.tiunk, 0)
 end)
 
-menu.toggle_loop(Money_instant, labels.Money_labels.ez680k_menu, {"680kloop"}, labels.Money_labels.ez_desc, function()
+menu.toggle_loop(Money, labels.Money_labels.ez680k_menu, {"680kloop"}, labels.Money_labels.ez_desc, function()
     main_var.loop_flag.is_loop_running = true
     labels.Watermark_label.loop_wlf.running_name = labels.Money_labels.ez680k_menu
 
     price = NETSHOPPING.NET_GAMESERVER_GET_PRICE(global_var.ezmoney_hash.bet, global_var.ezmoney_hash.category, true)
     if main_var.delay_Type == 1 then
-        do_ezmoney(global_var.ezmoney_hash.bet, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("680kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.bet, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
         util.yield(main_var.delay_sec.delay_1)
     elseif main_var.delay_Type == 2 then
-        do_ezmoney(global_var.ezmoney_hash.bet, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("680kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.bet, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
         util.yield(main_var.delay_sec.delay_1)
     else
-        do_ezmoney(global_var.ezmoney_hash.bet, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("680kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.bet, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
     end
 end, function()
     main_var.loop_flag.is_loop_running = false;
@@ -279,19 +342,37 @@ end, function()
     do_ezmoney(global_var.ezmoney_hash.tiunk, 0)
 end)
 
-menu.toggle_loop(Money_instant, labels.Money_labels.ez180k_menu, {"180kloop"}, labels.Money_labels.ez_desc, function()
+menu.toggle_loop(Money, labels.Money_labels.ez180k_menu, {"180kloop"}, labels.Money_labels.ez_desc, function()
     main_var.loop_flag.is_loop_running = true
     labels.Watermark_label.loop_wlf.running_name = labels.Money_labels.ez180k_menu
 
     price = NETSHOPPING.NET_GAMESERVER_GET_PRICE(global_var.ezmoney_hash.obj, global_var.ezmoney_hash.category, true)
     if main_var.delay_Type == 1 then
-        do_ezmoney(global_var.ezmoney_hash.obj, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("180kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.obj, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
         util.yield(main_var.delay_sec.delay_1)
     elseif main_var.delay_Type == 2 then
-        do_ezmoney(global_var.ezmoney_hash.obj, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("180kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.obj, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
         util.yield(main_var.delay_sec.delay_1)
     else
-        do_ezmoney(global_var.ezmoney_hash.obj, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("180kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.obj, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
     end
 end, function()
     main_var.loop_flag.is_loop_running = false;
@@ -299,19 +380,37 @@ end, function()
     do_ezmoney(global_var.ezmoney_hash.tiunk, 0)
 end)
 
-menu.toggle_loop(Money_instant, labels.Money_labels.ez50k_menu, {"50kloop"}, labels.Money_labels.ez_desc, function()
+menu.toggle_loop(Money, labels.Money_labels.ez50k_menu, {"50kloop"}, labels.Money_labels.ez_desc, function()
     main_var.loop_flag.is_loop_running = true
     labels.Watermark_label.loop_wlf.running_name = labels.Money_labels.ez50k_menu
 
     price = NETSHOPPING.NET_GAMESERVER_GET_PRICE(global_var.ezmoney_hash.tiunk, global_var.ezmoney_hash.category, true)
     if main_var.delay_Type == 1 then
-        do_ezmoney(global_var.ezmoney_hash.tiunk, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("50kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.tiunk, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
         util.yield(main_var.delay_sec.delay_1)
     elseif main_var.delay_Type == 2 then
-        do_ezmoney(global_var.ezmoney_hash.tiunk, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("50kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.tiunk, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
         util.yield(main_var.delay_sec.delay_1)
     else
-        do_ezmoney(global_var.ezmoney_hash.tiunk, price)
+        if(main_var.limit_flag.got >= main_var.limit_flag.limit_selection) then
+            util.toast(labels.Flag_label.done)
+            menu.trigger_commands("50kloop off")
+        else
+            do_ezmoney(global_var.ezmoney_hash.tiunk, price)
+            main_var.limit_flag.got = main_var.limit_flag.got + price
+        end
     end
 end, function()
     main_var.loop_flag.is_loop_running = false;
@@ -338,6 +437,41 @@ menu.toggle_loop(Misc, labels.Misc_labels.trans_atskp_menu, {"autoskiptans"}, la
 end, function()
     main_var.loop_flag.is_autoskip_running = false
 end)
+
+menu.action(Misc, labels.Misc_labels.fast_unlock, {"fastunlockall"}, labels.Misc_labels.fast_unlock_desc, function()
+    menu.trigger_commands("unlockall")
+    menu.trigger_commands("unlockprizeride")
+end)
+
+menu.divider(Misc, labels.Misc_labels.fast_change_rp_div)
+
+fast_rp_selection = menu.slider(Misc, labels.Misc_labels.fast_rp_sel_name, {"selectfastrp"}, labels.Misc_labels.fast_rp_sel_desc, 0, 8000, 120, 1, function()
+    main_var.fast_change.rp_selection = menu.get_value(fast_rp_selection)
+end)
+
+menu.action(Misc, labels.Misc_labels.fast_do_rp, {"fastchangerp"}, labels.Misc_labels.fast_do_rp_desc, function()
+    menu.trigger_commands("setrank "..main_var.fast_change.rp_selection)
+    util.yield(100)
+    menu.trigger_commands("go solo")
+end)
+
+menu.action(Misc, labels.Misc_labels.fast_do_rep, {"fastchangels"}, labels.Misc_labels.fast_do_rep_desc, function()
+    menu.trigger_commands("setlscarmeetreplevel "..main_var.fast_change.rp_selection)
+end)
+
+menu.divider(Misc, labels.Misc_labels.limit_div)
+
+earn_limiter = menu.slider(Misc, labels.Misc_labels.limit_sel, {"selectearnlimit"}, labels.Misc_labels.limit_sel_de, 0, 2147483647, 50000000, 1000000, function()
+    main_var.limit_flag.limit_selection = menu.get_value(earn_limiter)
+end)
+
+menu.action(Misc, labels.Misc_labels.reset_earn, {"resetearned"}, "", function()
+    main_var.limit_flag.got = 0
+end)
+
+menu.toggle(Misc, labels.Misc_labels.limit_enable, {"earn limit"}, labels.Misc_labels.limit_enable_desc, function(on)
+    main_var.limit_flag.is_limit = on
+end, true)
 
 ---#Setting
 menu.divider(Setting, labels.Setting_labels.delay_select_div)
@@ -368,14 +502,9 @@ delay_1_selection = menu.slider(Delay, labels.Setting_labels.delay_custom_select
     main_var.delay_sec.delay_1 = menu.get_value(delay_1_selection)
 end)
 
-delay_2_selection = menu.slider(Delay, labels.Setting_labels.delay_custom_select_2, {"selectdelay2"}, labels.Setting_labels.delay_custom_select_2_desc, 0, 10000000, 1000, 100, function()
-    --util.toast("New Delay 2: " ..main_var.delay_sec.delay_2)
-    main_var.delay_sec.delay_2 = menu.get_value(delay_2_selection)
-end)
-
-delay_3_selection = menu.slider(Delay, labels.Setting_labels.delay_custom_select_3, {"selectdelay3"}, labels.Setting_labels.delay_custom_select_3_desc, 0, 100000000, 10000, 500, function()
+delay_2_selection = menu.slider(Delay, labels.Setting_labels.delay_custom_select_4, {"selectdelay4"}, labels.Setting_labels.delay_custom_select_3_desc, 0, 100000000, 11500, 500, function()
     --util.toast("New Delay 3: " ..main_var.delay_sec.delay_3)
-    main_var.delay_sec.delay_3 = menu.get_value(delay_3_selection)
+    main_var.delay_sec.delay_2 = menu.get_value(delay_2_selection)
 end)
 
 menu.divider(Setting, labels.Setting_labels.overlay_div)
@@ -419,6 +548,12 @@ util.create_tick_handler(function()
         else
             util.draw_debug_text(labels.Watermark_label.loop_wlf.autoskip_off)
         end
+
+        util.draw_debug_text(labels.Watermark_label.none)
+
+        util.draw_debug_text(labels.Watermark_label.limit_wlf.limited_earn)
+        util.draw_debug_text(labels.Watermark_label.limit_wlf.your_earned)
+        util.draw_debug_text(labels.Watermark_label.limit_wlf.is_limit_wlf)
     end
 end)
 
